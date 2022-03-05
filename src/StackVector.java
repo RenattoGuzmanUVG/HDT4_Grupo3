f06ca13fca36f4634dde753df28dff0c8be85ace
AragonD19 <88000475+AragonD19@users.noreply.github.com>
2022-03-04 23:05:53 -0600

    Update StackVector.java

--- src/StackVector.java
+++ src/StackVector.java
@@ -1,2 +1,57 @@
-public class StackVector extends Stack{
+import java.util.ArrayList;
+import java.util.*;
+
+public class StackVector<T> extends Stack<T>{
+    private Vector miVector;
+
+    public StackVector() {
+        miVector = new Vector<T>();
+    }
+
+    /**
+     * Añade un valor a la pila en la primer posición.
+     * @param value Valor a agregar.
+     */
+
+    public void push(T value) {
+        miVector.add(0, value);
+    }
+
+    /**
+     * Elimina el elemento de la pila en la posición cero.
+     * @return El elemento removido
+     */
+
+    public T pull() {
+        return (T) miVector.remove(0);
+    }
+
+    /**
+     * Obtiene el valor del elemento de la posición cero de la pila
+     * @return El elemento obtenido.
+     */
+    @Override
+    public T peek() {
+        return (T) miVector.get(0);
+    }
+
+    /**
+     * Cuenta la cantidad de datos que tiene la pila en ese momento.
+     * @return El número de elementos de la pila.
+     */
+
+    public int count() {
+        return miVector.size();
+    }
+
+    /**
+     * Verifica si la pila se encuentra vacia.
+     * @return True = Vacia, False = Contiene elementos.
+     */
+
+    public boolean isEmpty() {
+        return miVector.isEmpty();
+    }
+
 }
+
