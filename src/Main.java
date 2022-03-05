81ac634a4788502e55e0b7dbf0f34ca773292970
Diego Hernández <dahernandezsilvestre@gmail.com>
2022-03-04 22:12:19 -0600

    Menú
    Implementación del menú de interacción con el usuario.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,83 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner s = new Scanner(System.in);
+        Lectura l = new Lectura();
+        CalcIInPostFix c = new CalcIInPostFix();
+
+        int seleccion;
+        String expresion;
+        int resultado;
+
+        expresion = l.lector("datos");
+
+        System.out.println("""
+                ██╗███╗░░██╗███████╗██╗██╗░░██╗  ████████╗░█████╗░
+                ██║████╗░██║██╔════╝██║╚██╗██╔╝  ╚══██╔══╝██╔══██╗
+                ██║██╔██╗██║█████╗░░██║░╚███╔╝░  ░░░██║░░░██║░░██║
+                ██║██║╚████║██╔══╝░░██║░██╔██╗░  ░░░██║░░░██║░░██║
+                ██║██║░╚███║██║░░░░░██║██╔╝╚██╗  ░░░██║░░░╚█████╔╝
+                ╚═╝╚═╝░░╚══╝╚═╝░░░░░╚═╝╚═╝░░╚═╝  ░░░╚═╝░░░░╚════╝░
+                                              
+                ██████╗░░█████╗░░██████╗████████╗███████╗██╗██╗░░██╗
+                ██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔════╝██║╚██╗██╔╝
+                ██████╔╝██║░░██║╚█████╗░░░░██║░░░█████╗░░██║░╚███╔╝░
+                ██╔═══╝░██║░░██║░╚═══██╗░░░██║░░░██╔══╝░░██║░██╔██╗░
+                ██║░░░░░╚█████╔╝██████╔╝░░░██║░░░██║░░░░░██║██╔╝╚██╗
+                ╚═╝░░░░░░╚════╝░╚═════╝░░░░╚═╝░░░╚═╝░░░░░╚═╝╚═╝░░╚═╝
+                
+                █▀▀ ▄▀█ █░░ █▀▀ █░█ █░░ ▄▀█ ▀█▀ █▀█ █▀█
+                █▄▄ █▀█ █▄▄ █▄▄ █▄█ █▄▄ █▀█ ░█░ █▄█ █▀▄
+                """);
+        System.out.println("De acuerdo, démosle una solución a sus problemas...");
+        System.out.println("""
+                
+                Seleccione un tipo de stack a implementar:
+                [1]. ArrayList.
+                [2]. Vector.
+                [3]. Lista.""");
+        seleccion = s.nextInt();
+        s.nextLine();
+        if (seleccion == 3){
+            System.out.println("""
+                
+                Ya que seleccionó el tipo de stack 'Lista', seleccione un tipo de implementación:
+                [4]. Lista simplemente encadenada
+                [5]. Lista doblemente encadenada.""");
+            seleccion = s.nextInt();
+            s.nextLine();
+        }
+
+        System.out.println("""
+                Perfecto, ahora procederemos a calcular la expresión que se encuentra en su archivo...
+                
+                Presione una tecla para continuar!""");
+        s.nextLine();
+
+        switch (seleccion){
+            case 1:
+                StackArrayList a = new StackArrayList();
+                break;
+            case 2:
+                StackVector v = new StackVector();
+                break;
+            case 4:
+                SingleStackList singlel = new SingleStackList();
+                break;
+            case 5:
+                DoubleStackList doublel = new DoubleStackList();
+                break;
+        }
+
+        resultado = c.EvaluateInFix(expresion);
+        resultado = c.EvaluatePostFix(expresion);
+
+        System.out.println("Archivo escaneado!");
+        System.out.println("El resultado de evaluar la expresion '" + expresion + "' es: ");
+        System.out.println("Solicitud finalizada con éxito. Finalizando programa...");
+
     }
 }
--- src/StackArrayList.java
+++ src/StackArrayList.java
@@ -8,4 +8,49 @@ public class StackArrayList<T> extends Stack<T> {
         miListaInterna = new ArrayList<T>();
     }
 
+    /**
+     * Añade un valor a la pila en la primer posición.
+     * @param value Valor a agregar.
+     */
+    @Override
+    public void push(T value) {
+        miListaInterna.add(0, value);
+    }
+
+    /**
+     * Elimina el elemento de la pila en la posición cero.
+     * @return El elemento removido
+     */
+    @Override
+    public T pull() {
+        return miListaInterna.remove(0);
+    }
+
+    /**
+     * Obtiene el valor del elemento de la posición cero de la pila
+     * @return El elemento obtenido.
+     */
+    @Override
+    public T peek() {
+        return miListaInterna.get(0);
+    }
+
+    /**
+     * Cuenta la cantidad de datos que tiene la pila en ese momento.
+     * @return El número de elementos de la pila.
+     */
+    @Override
+    public int count() {
+        return miListaInterna.size();
+    }
+
+    /**
+     * Verifica si la pila se encuentra vacia.
+     * @return True = Vacia, False = Contiene elementos.
+     */
+    @Override
+    public boolean isEmpty() {
+        return miListaInterna.isEmpty();
+    }
+
 }
