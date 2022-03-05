55fcd0d4abca419b9403e9dbfdf237c052266411
Diego Hernández <dahernandezsilvestre@gmail.com>
2022-03-04 20:25:52 -0600

    Lectura
    Clase que permite leer archivos implementada.

--- src/Lectura.java
+++ src/Lectura.java
@@ -1,2 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Lectura {
+    public String lector(String path){
+        String texto="";
+        try{
+            BufferedReader bf = new BufferedReader(new FileReader(path));
+            String temp = "";
+            String bflinea;
+            while ((bflinea = bf.readLine()) != null){
+                temp = texto + bflinea;
+            }
+            texto = temp;
+        } catch (Exception e){
+            System.out.println("[ERROR]. El archivo no fue encontrado");
+        }
+        return texto;
+    }
 }
